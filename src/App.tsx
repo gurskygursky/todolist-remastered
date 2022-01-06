a635a1bc7ba2f4d9e60c720ffab71877e4d898a8
gurskygursky <yahorhurski@gmail.com>
2022-01-06 18:49:57 +0300

    use hook useState to tasks array

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
 export function App() {
 
     const removeTask = (id: number) => {
-        todolist1 = todolist1.filter(task => task.id !== id)
-        console.log(todolist1, todolist2)
+        const FilteredTask = tasks.filter(task => task.id !== id)
+        setTask(FilteredTask);
+        console.log(tasks)
     }
 
-
-    let todolist1 = [
+    let [tasks, setTask] = useState([
         {id: 1, taskTitle: "HTML & CSS", isDone: true},
         {id: 2, taskTitle: "Javascript", isDone: false},
         {id: 3, taskTitle: "Typescript", isDone: true},
         {id: 4, taskTitle: "React", isDone: true},
         {id: 5, taskTitle: "Rest API", isDone: true},
         {id: 6, taskTitle: "Redux", isDone: true},
-    ]
-    let todolist2 = [
-        {id: 1, taskTitle: "1984", isDone: true},
-        {id: 2, taskTitle: "The Financier", isDone: true},
-        {id: 3, taskTitle: "The Stoic", isDone: true},
-        {id: 4, taskTitle: "The Titan", isDone: true},
-        {id: 5, taskTitle: "The Double", isDone: true},
-        {id: 6, taskTitle: "The Master and Margarita", isDone: false},
-    ]
+    ]);
+    // let todolist2 = [
+    //     {id: 1, taskTitle: "1984", isDone: true},
+    //     {id: 2, taskTitle: "The Financier", isDone: true},
+    //     {id: 3, taskTitle: "The Stoic", isDone: true},
+    //     {id: 4, taskTitle: "The Titan", isDone: true},
+    //     {id: 5, taskTitle: "The Double", isDone: true},
+    //     {id: 6, taskTitle: "The Master and Margarita", isDone: false},
+    // ]
     return (
         <div className="App">
-            <Todolist todolistTitle={'What to learn 111'} tasks={todolist1} removeTask={removeTask}/>
-            <Todolist todolistTitle={'What to learn 222'} tasks={todolist2} removeTask={removeTask}/>
+            <Todolist todolistTitle={'What to learn'} tasks={tasks} removeTask={removeTask}/>
+            {/*<Todolist todolistTitle={'Reading list'} tasks={tasks} removeTask={removeTask}/>*/}
         </div>
     );
 }
