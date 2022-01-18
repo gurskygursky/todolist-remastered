712b7b8bfd02e393765bd5f2d909ba473893c932
gurskygursky <yahorhurski@gmail.com>
2022-01-18 22:31:30 +0300

    create user tests

---
+++ src/state/user-reducer.ts
@@ -0,0 +1,36 @@
+type StateType = {
+    age: number
+    childrenCount: number
+    name: string
+}
+type ActionType = {
+    type: string
+    [key: string]: any
+}
+
+export const userReducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+        case 'INCREMENT-AGE': {
+            let newState = {...state};
+            newState.age = state.age + 1;
+            return newState;
+            // state.age = state.age + 1;
+            // return state;
+        }
+        case 'INCREMENT-CHILDREN-COUNT':
+            return {
+                ...state,
+                childrenCount: state.childrenCount + 1
+            }
+        // state.childrenCount = state.childrenCount + 1;
+        // return state;
+        case 'CHANGE-NAME': {
+            let newState = {...state};
+            newState.name = 'Viktor';
+            return newState;
+        }
+
+        default:
+            throw new Error("I don't understand this type")
+    }
+}
