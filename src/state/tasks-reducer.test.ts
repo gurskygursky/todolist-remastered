09ef13c92c16c474e02d087945784968c4a20a2b
gurskygursky <yahorhurski@gmail.com>
2022-03-28 16:47:53 +0300

    refactoring tasks-reducer test

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,223 +1,147 @@
-export {};
-// import {v1} from "uuid";
-// import {TasksStateType, TaskType, TodolistType} from "../Todolist";
-// import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./tasks-reducer";
-// import {addTodolistAC, removeTodolistAC} from "./todolists-reducer";
-//
-// // let todolistID1: string;
-// // let todolistID2: string;
-// let startState: TasksStateType = {};
-//
-// beforeEach(() => {
-//     // todolistID1: v1();
-//     // todolistID2: v1();
-//
-//     startState = {
-//         todolistID1: [
-//             {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//             {id: "2", taskTitle: "Javascript", isDone: false},
-//             {id: "3", taskTitle: "Typescript", isDone: true},
-//             {id: "4", taskTitle: "React", isDone: true},
-//             {id: "5", taskTitle: "Rest API", isDone: true},
-//             {id: "6", taskTitle: "Redux", isDone: true},
-//         ],
-//         todolistID2: [
-//             {id: "1", taskTitle: "1984", isDone: true},
-//             {id: "2", taskTitle: "The Financier", isDone: true},
-//             {id: "3", taskTitle: "The Stoic", isDone: true},
-//             {id: "4", taskTitle: "The Titan", isDone: true},
-//             {id: "5", taskTitle: "The Double", isDone: true},
-//             {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//         ],
-//     }
-// })
-//
-// test('correct task should be deleted from correct array', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//
-//     const action = removeTaskAC("todolistID2", "2");
-//     const endState = tasksReducer(startState, action);
-//
-//     expect(endState).toEqual({
-//         todolistID1: [
-//             {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//             {id: "2", taskTitle: "Javascript", isDone: false},
-//             {id: "3", taskTitle: "Typescript", isDone: true},
-//             {id: "4", taskTitle: "React", isDone: true},
-//             {id: "5", taskTitle: "Rest API", isDone: true},
-//             {id: "6", taskTitle: "Redux", isDone: true},
-//         ],
-//         todolistID2: [
-//             {id: "1", taskTitle: "1984", isDone: true},
-//             {id: "3", taskTitle: "The Stoic", isDone: true},
-//             {id: "4", taskTitle: "The Titan", isDone: true},
-//             {id: "5", taskTitle: "The Double", isDone: true},
-//             {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//         ]
-//     });
-//
-// });
-// test('correct task should be added to correct array', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//     const action = addTaskAC("todolistID2", "juice");
-//     const endState = tasksReducer(startState, action);
-//
-//     expect(endState["todolistID1"].length).toBe(6);
-//     expect(endState["todolistID2"].length).toBe(7);
-//     // expect(endState["todolistID2"][0].id).toBe();
-//     expect(endState["todolistID2"][0].taskTitle).toBe("juice");
-//     expect(endState["todolistID2"][0].isDone).toBe(false);
-//     expect(endState["todolistID2"][1].taskTitle).toBe("1984");
-//
-// });
-// test('status of specified task should be changed', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//
-//     const action = changeTaskStatusAC("todolistID2", "2", false,);
-//     const endState = tasksReducer(startState, action);
-//
-//     expect(endState["todolistID2"][1].isDone).toBe(false);
-//     expect(endState["todolistID1"][1].isDone).toBe(false);
-//     expect(endState["todolistID2"][1].taskTitle).toBe("The Financier");
-// });
-// test('title of specified task should be changed', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//
-//     const action = changeTaskTitleAC("todolistID2", "2", "Crime and Punishment",);
-//
-//     const endState = tasksReducer(startState, action);
-//
-//     expect(endState["todolistID2"][1].isDone).toBe(true);
-//     expect(endState["todolistID1"][1].isDone).toBe(false);
-//     expect(endState["todolistID2"][1].taskTitle).toBe("Crime and Punishment");
-// });
-// test('new array should be added when new todolist is added', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//
-//     const action = addTodolistAC("new todolist");
-//     const endState = tasksReducer(startState, action);
-//
-//     const keys = Object.keys(endState);
-//     const newKey = keys.find(k => k !== "todolistID1" && k !== "todolistID2");
-//     if (!newKey) {
-//         throw Error("new key should be added")
-//     }
-//
-//     expect(keys.length).toBe(3);
-//     expect(endState[newKey]).toEqual([]);
-// });
-// test('property with todolistId should be deleted', () => {
-//     // const startState: TasksStateType = {
-//     //     "todolistID1": [
-//     //         {id: "1", taskTitle: "HTML & CSS", isDone: true},
-//     //         {id: "2", taskTitle: "Javascript", isDone: false},
-//     //         {id: "3", taskTitle: "Typescript", isDone: true},
-//     //         {id: "4", taskTitle: "React", isDone: true},
-//     //         {id: "5", taskTitle: "Rest API", isDone: true},
-//     //         {id: "6", taskTitle: "Redux", isDone: true},
-//     //     ],
-//     //     "todolistID2": [
-//     //         {id: "1", taskTitle: "1984", isDone: true},
-//     //         {id: "2", taskTitle: "The Financier", isDone: true},
-//     //         {id: "3", taskTitle: "The Stoic", isDone: true},
-//     //         {id: "4", taskTitle: "The Titan", isDone: true},
-//     //         {id: "5", taskTitle: "The Double", isDone: true},
-//     //         {id: "6", taskTitle: "The Master and Margarita", isDone: false},
-//     //     ],
-//     // };
-//
-//     const action = removeTodolistAC("todolistID2");
-//     const endState = tasksReducer(startState, action);
-//
-//     const keys = Object.keys(endState);
-//
-//     expect(keys.length).toBe(1);
-//     expect(endState["todolistID2"]).not.toBeDefined();
-// });
-//
-//
+import {TasksStateType} from "../features/todolists/todolist/Todolist";
+import {addTaskAC, removeTaskAC, tasksReducer, updateTaskAC} from "./tasks-reducer";
+import {addTodolistAC, removeTodolistAC} from "./todolists-reducer";
+import {TaskPriorities, TaskStatuses} from "../api/todolists-api";
+
+
+let startState: TasksStateType = {};
+
+beforeEach(() => {
+    startState = {
+        'todolistID1': [
+            {
+                id: "1", title: "HTML & CSS", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "2", title: "Javascript", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "3", title: "Typescript", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "4", title: "React", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "5", title: "Rest API", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "6", title: "Redux", status: TaskStatuses.New, todoListId: 'todolistID1',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+        ],
+        'todolistID2': [
+            {
+                id: "1", title: "1984", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "2", title: "The Financier", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "3", title: "The Stoic", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "4", title: "The Titan", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "5", title: "The Double", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+            {
+                id: "6", title: "The Master and Margarita", status: TaskStatuses.New, todoListId: 'todolistID2',
+                priority: TaskPriorities.Low, deadline: '', startDate: '', addedDate: '', order: 0, description: ''
+            },
+        ],
+    }
+})
+
+test('correct task should be deleted from correct array', () => {
+    const action = removeTaskAC({todolistID: "todolistID2", taskID: "2"});
+    const endState = tasksReducer(startState, action);
+
+    expect(endState['todolistID1'].length).toBe(6);
+    expect(endState['todolistID2'].length).toBe(5);
+    expect(endState['todolistID1'].every(t => t.id !== '2')).toBeTruthy();
+
+});
+test('correct task should be added to correct array', () => {
+    const task = {
+        id: "1",
+        title: "juice",
+        status: TaskStatuses.New,
+        todoListId: 'todolistID2',
+        priority: TaskPriorities.Low,
+        deadline: '',
+        startDate: '',
+        addedDate: '',
+        order: 0,
+        description: '',
+    }
+    const action = addTaskAC({task: task});
+    const endState = tasksReducer(startState, action);
+
+    expect(endState["todolistID1"].length).toBe(6);
+    expect(endState["todolistID2"].length).toBe(7);
+    expect(endState["todolistID2"][0].title).toBe("juice");
+    expect(endState["todolistID2"][0].status).toBe(TaskStatuses.New);
+    expect(endState["todolistID2"][1].title).toBe("1984");
+    expect(endState["todolistID2"][6].title).toBe("The Master and Margarita");
+
+});
+test('status of specified task should be changed', () => {
+    const action = updateTaskAC({todolistID: "todolistID2", taskID: "2", domainModel: {status: TaskStatuses.Completed}});
+    const endState = tasksReducer(startState, action);
+
+    expect(endState["todolistID2"][1].status).toBe(TaskStatuses.Completed);
+    expect(endState["todolistID1"][1].status).toBe(TaskStatuses.New);
+    expect(endState["todolistID2"][1].title).toBe("The Financier");
+});
+test('title of specified task should be changed', () => {
+    const action = updateTaskAC({todolistID: "todolistID2", taskID: "2", domainModel: {title: "Crime and Punishment"}});
+
+    const endState = tasksReducer(startState, action);
+
+    expect(endState["todolistID2"][1].status).toBe(TaskStatuses.New);
+    expect(endState["todolistID1"][1].status).toBe(TaskStatuses.New);
+    expect(endState["todolistID2"][1].title).toBe("Crime and Punishment");
+});
+test('new array should be added when new todolist is added', () => {
+    const todolist = {
+        id: '2',
+        addedDate: '',
+        order: 0,
+        title: 'todolistID3',
+    }
+    const action = addTodolistAC({todolist: todolist});
+    const endState = tasksReducer(startState, action);
+
+    const keys = Object.keys(endState);
+    const newKey = keys.find(k => k !== "todolistID1" && k !== "todolistID2");
+    if (!newKey) {
+        throw Error("new key should be added")
+    }
+
+    expect(keys.length).toBe(3);
+    expect(endState[keys[3]]).toEqual(undefined)
+    expect(endState[newKey]).toEqual([]);
+});
+test('property with todolistId should be deleted', () => {
+
+    const action = removeTodolistAC({todolistID: "todolistID2"});
+    const endState = tasksReducer(startState, action);
+
+    const keys = Object.keys(endState);
+
+    expect(keys.length).toBe(1);
+    expect(endState["todolistID2"]).not.toBeDefined();
+});
+
+
